01e7073ce3e7b14934c52276df2c118fc055f839
Me <schmierfink@yandex.ru>
2021-12-15 00:10:20 +0100

    encode objects

--- TestPWA/wwwroot/Checklist2/ts/dev/urlEncoder.js
+++ TestPWA/wwwroot/Checklist2/ts/dev/urlEncoder.js
@@ -22,17 +22,36 @@ function urlEncode(str) {
 function isPrimitive(val) {
     return !(typeof val == "object" || typeof val == "function");
 }
+function isArray(obj) {
+    return !!obj && obj.constructor === Array;
+}
 function objectToQueryString(obj) {
     var str = [];
     for (var p in obj) {
         if (obj.hasOwnProperty(p)) {
-            if (isPrimitive(obj[p]))
+            if (isPrimitive(obj[p])) {
                 str.push(urlEncode(p) + "=" + urlEncode(obj[p]));
+            }
+            else if (isArray(obj[p])) {
+                var t = obj[p];
+                for (var i = 0; i < t.length; ++i) {
+                    str.push(urlEncode(p) + "=" + urlEncode(t[i]));
+                }
+            }
         }
     }
     return str.join("&");
 }
-var person = { first_name: "Marty", last_name: "Mcfly" };
+var person = {
+    "first_name": "Marty",
+    "last_name": "Mcfly",
+    "dependencies": ["stack", "queue", "linked_list"],
+    "objects": {
+        "key1": "value 1",
+        "key2": "value 2",
+        "key3": "value 3"
+    }
+};
 var queryString = objectToQueryString(person);
 function nestedLoop(obj) {
     var res = {};
